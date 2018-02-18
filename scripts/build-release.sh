83e770c7d46b7092ab82dda394a65d76729aa21a
Nicholas Bering <bering.nicholas@gmail.com>
2018-02-18 15:29:52 -0500

    Fix release binary filename.
    Closes #2

--- scripts/build-release.sh
+++ scripts/build-release.sh
@@ -7,7 +7,7 @@ rm -r pkg/*
 PROJECT_NAME=${PWD##*/}
 RELEASE_VERSION="$(git describe --abbrev=0)"
 
-gox -output "pkg/{{.OS}}_{{.Arch}}/{{.Dir}}-${RELEASE_VERSION}"
+gox -output "pkg/{{.OS}}_{{.Arch}}/{{.Dir}}_${RELEASE_VERSION}"
 
 cd pkg;
 find * -type d -exec zip -r "${PROJECT_NAME}-{}.zip" "{}" \;

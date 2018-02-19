9bdb581ab1e59fe867eb97844b770a1390c9eac0
Nicholas Bering <bering.nicholas@gmail.com>
2018-02-19 12:11:09 -0500

    Install zip if not present.

--- scripts/build-release.sh
+++ scripts/build-release.sh
@@ -8,6 +8,22 @@ which gox
 if [[ $? -ne 0 ]]; then
     go get github.com/mitchellh/gox
 fi
+
+which gox
+if [[ $? -ne 0 ]]; then
+    which dpkg
+    if [[ $? -ne 0 ]]; then
+        echo "Missing zip utility, and no package install strategy available." >&2
+        exit 1
+    else
+        set -e
+        pushd "$(mktemp -d)"
+        wget https://deb.debian.org/debian/pool/main/z/zip/zip_3.0-11+b1_amd64.deb
+        dpkg -i zip_3.0-11+b1_amd64.deb
+        rm zip_3.0-11+b1_amd64.deb
+        popd
+    fi
+fi
 set -e
 
 # Clean old builds if present.

22099b5b7468f5f014eb19bca7aa8e8db3f5eb55
Joe Topjian <joe@topjian.net>
2018-02-25 21:48:07 +0000

    Add ansible_group resource

---
+++ ansible/resource_group.go
@@ -0,0 +1,40 @@
+package ansible
+
+import (
+	"github.com/hashicorp/terraform/helper/schema"
+)
+
+func resourceGroup() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceAnsibleInventoryGroupCreate,
+		Read:   schema.Noop,
+		Update: schema.Noop,
+		Delete: schema.Noop,
+
+		Schema: map[string]*schema.Schema{
+			"inventory_group_name": {
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+
+			"children": {
+				Type: schema.TypeList,
+				Elem: &schema.Schema{
+					Type: schema.TypeString,
+				},
+				Optional: true,
+			},
+
+			"vars": {
+				Type:     schema.TypeMap,
+				Optional: true,
+			},
+		},
+	}
+}
+
+func resourceAnsibleInventoryGroupCreate(d *schema.ResourceData, _ interface{}) error {
+	d.SetId(d.Get("inventory_group_name").(string))
+	return nil
+}

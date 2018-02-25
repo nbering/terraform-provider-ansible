a27abe0abe42538f8a2a1d912bc986adcc78b590
Nicholas Bering <bering.nicholas@gmail.com>
2018-02-24 23:58:46 -0500

    Limit OS and Architecture combos to reduce build times.

--- scripts/build-release.sh
+++ scripts/build-release.sh
@@ -34,7 +34,11 @@ fi
 PROJECT_NAME=${PWD##*/}
 RELEASE_VERSION="$(git describe --abbrev=0)"
 
-gox -output "pkg/{{.OS}}_{{.Arch}}/{{.Dir}}_${RELEASE_VERSION}"
+gox \
+    -os="linux darwin windows freebsd openbsd solaris" \
+    -arch="386 amd64 arm" \
+    -osarch="!darwin/arm !darwin/386" \
+    -output "pkg/{{.OS}}_{{.Arch}}/{{.Dir}}_${RELEASE_VERSION}"
 
 cd pkg;
 find * -type d -exec zip -r "${PROJECT_NAME}-{}.zip" "{}" \;

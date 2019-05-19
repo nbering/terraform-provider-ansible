12e954b7fb09380631176016d92b55dbefbf8db9
Nicholas Bering <bering.nicholas@gmail.com>
2019-05-19 17:28:40 -0400

    Basic support for "ansible_host_var" as a resource.

---
+++ ansible/resource_host_var_test.go
@@ -0,0 +1,36 @@
+package ansible
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform/helper/resource"
+)
+
+func TestAnsibleHostVar(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config: testAnsibleHostVarConfig,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(
+						"ansible_host_var.hostvar_1", "id", "www.example.com/foo"),
+					resource.TestCheckResourceAttr(
+						"ansible_host_var.hostvar_1", "inventory_hostname", "www.example.com"),
+					resource.TestCheckResourceAttr(
+						"ansible_host_var.hostvar_1", "key", "foo"),
+					resource.TestCheckResourceAttr(
+						"ansible_host_var.hostvar_1", "value", "bar"),
+				),
+			},
+		},
+	})
+}
+
+const testAnsibleHostVarConfig = `
+  resource "ansible_host_var" "hostvar_1" {
+	  inventory_hostname = "www.example.com"
+	  key                = "foo"
+	  value              = "bar"
+  }
+`

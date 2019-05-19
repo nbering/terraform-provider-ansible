6361ca4a8440f19b56ba0ae4a00e7839da8df682
Nicholas Bering <bering.nicholas@gmail.com>
2019-05-19 17:45:35 -0400

    Simple implementation of "ansible_group_var" resource.

---
+++ ansible/resource_group_var.go
@@ -0,0 +1,40 @@
+package ansible
+
+import (
+	"fmt"
+
+	"github.com/hashicorp/terraform/helper/schema"
+)
+
+func resourceGroupVar() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceAnsibleGroupVarCreate,
+		Read:   schema.Noop,
+		Update: schema.Noop,
+		Delete: schema.Noop,
+
+		Schema: map[string]*schema.Schema{
+			"inventory_group_name": {
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+
+			"key": {
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+
+			"value": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+		},
+	}
+}
+
+func resourceAnsibleGroupVarCreate(d *schema.ResourceData, _ interface{}) error {
+	d.SetId(fmt.Sprintf("%s/%s", d.Get("inventory_group_name"), d.Get("key")))
+	return nil
+}
